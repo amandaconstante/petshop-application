4e1d978866fed7a583e45a7f52429df0eeb3e6ba
Amanda <89360779+amandaconstante@users.noreply.github.com>
2025-08-06 21:41:45 -0300

    disabled CSRF protection

---
+++ src/main/java/com/app/petshop/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.app.petshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Desabilita CSRF para facilitar teste com POST
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Libera acesso a todos os endpoints
+                );
+
+        return http.build();
+    }
+}
