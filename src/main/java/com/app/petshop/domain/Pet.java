2c0b04d417f5fa1177914b65de238e7cfedaf9d4
Amanda <89360779+amandaconstante@users.noreply.github.com>
2025-08-08 21:56:54 -0300

    small changes

--- src/main/java/com/app/petshop/domain/Pet.java
+++ src/main/java/com/app/petshop/domain/Pet.java
@@ -1,5 +1,6 @@
 package com.app.petshop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,6 +25,7 @@ public class Pet implements Serializable {
     @Enumerated(EnumType.STRING)
     private PetType type;
     private int age;
+    @JsonIgnore // should be ignored during the serialization and deserialization processes to and from JSON.
     @ManyToOne
     @JoinColumn(name = "tutor_id") // cria a foreign key na tabela PET apontando para TUTOR
     private Tutor tutor;
