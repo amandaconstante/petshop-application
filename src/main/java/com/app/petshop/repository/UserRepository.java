00a64535f6f255ce78bc90933edf79544acf5cae
Amanda <89360779+amandaconstante@users.noreply.github.com>
2025-09-29 23:13:59 -0300

    add findByLogin method

--- src/main/java/com/app/petshop/repository/UserRepository.java
+++ src/main/java/com/app/petshop/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.app.petshop.repository;
 
-import com.app.petshop.domain.User;
+import com.app.petshop.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<Object> findByEmail(String email);
+
+    UserDetails findByLogin(String login);
 }
