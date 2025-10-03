bbdd611a1d6bcee05431ece5f6644d1e2f10f484
Amanda <89360779+amandaconstante@users.noreply.github.com>
2025-10-02 23:32:46 -0300

    add findById endpoint

--- src/main/java/com/app/petshop/controller/UserController.java
+++ src/main/java/com/app/petshop/controller/UserController.java
@@ -3,28 +3,27 @@ package com.app.petshop.controller;
 import com.app.petshop.domain.user.User;
 import com.app.petshop.dto.UserRequestDto;
 import com.app.petshop.dto.UserResponseDto;
+import com.app.petshop.repository.UserRepository;
 import com.app.petshop.service.UserService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping("/create")
-    public ResponseEntity<UserResponseDto> create(@RequestBody UserRequestDto userRequestDto) {
-        User user = userService.createUser(userRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new UserResponseDto(
-                        user.getId(), user.getName(), user.getLogin(), user.getBirthDate(), user.getEmail())
-                );
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userRepository.findById(id);
     }
 
 }
