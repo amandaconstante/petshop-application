5d35581e8798a7845cd62bb7ee2f108a95143dfe
Amanda <89360779+amandaconstante@users.noreply.github.com>
2025-10-02 23:32:12 -0300

    fix login and create endpoint into AuthenticationController

--- src/main/java/com/app/petshop/controller/AuthenticationController.java
+++ src/main/java/com/app/petshop/controller/AuthenticationController.java
@@ -1,8 +1,14 @@
 package com.app.petshop.controller;
 
 import com.app.petshop.domain.user.AuthenticationDto;
+import com.app.petshop.domain.user.User;
+import com.app.petshop.dto.UserRequestDto;
+import com.app.petshop.dto.UserResponseDto;
+import com.app.petshop.repository.UserRepository;
+import com.app.petshop.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,11 +23,29 @@ public class AuthenticationController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDto data) {
-        var userNamePassowrd = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var auth = authenticationManager.authenticate(userNamePassowrd);
+        var usernamePassowrd = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = authenticationManager.authenticate(usernamePassowrd);
+        if(auth.isAuthenticated()) {
+            System.out.println("Usuário autenticado!");
+
+        } else {
+            System.out.println("Usuário ou senha incorretos.");
+        }
 
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<UserResponseDto> create(@RequestBody UserRequestDto userRequestDto) {
+        User user = userService.createUser(userRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new UserResponseDto(
+                        user.getId(), user.getName(), user.getLogin(), user.getBirthDate(), user.getEmail())
+                );
     }
 }
